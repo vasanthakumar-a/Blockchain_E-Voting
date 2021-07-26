e3b44472098043b9b0705d7eec1c61559c4f5336
Vasanthakumar A <vasanthakumara117@gmail.com>
2021-07-26 16:31:01 +0530

    init

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,10 @@ urlpatterns=[
     path('userLogin',views.userLogin,name='userLogin'),
     path('userRegister',views.userRegister,name='userRegister'),
     path('adminLogin',views.adminLogin,name='adminLogin'),
+    path('main',views.main,name='main'),
+    path('result',views.result,name='result'),
+    path('vote',views.vote,name='vote'),
+    path('conform1',views.conform1,name='conform1'),
+    path('conform2',views.conform2,name='conform2'),
+    path('conform3',views.conform3,name='conform3'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,20 @@
+import json
 from django.shortcuts import render
+from eth_utils import address
+import web3
+from web3 import Web3
 
 # Create your views here.
+
+# ganache_url = "https://127.0.0.1:7545"
+# web3 = Web3(Web3.HTTPProvider(ganache_url))
+# web3.eth.default_account = web3.eth.accounts[0]
+# abi = json.loads('')
+# address = web3.toChecksumAddress("")
+
+# contract = web3.eth.contract(address=address, abi=abi)
+
+cand = 0
 
 def index(request):
     return render(request,'index.html')
@@ -13,3 +27,29 @@ def userRegister(request):
 
 def adminLogin(request):
     return render(request, 'adminLogin.html')
+
+def main (request):
+    return render(request, 'admin.html')
+
+def result(request):
+    return render(request, 'result.html')
+
+def vote(request):
+    global cand
+    cand = 0
+    return render(request, 'vote.html',{'cand':cand})
+
+def conform1(request):
+    global cand
+    cand = 1
+    return render(request, 'conform.html',{'cand':cand})
+
+def conform2(request):
+    global cand
+    cand = 2
+    return render(request, 'conform.html',{'cand':cand})
+
+def conform3(request):
+    global cand
+    cand = 3
+    return render(request, 'conform.html',{'cand':cand})
