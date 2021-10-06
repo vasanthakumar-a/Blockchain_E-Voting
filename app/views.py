352ad9f458dd7609ce018103cf096ea4d1b0bd47
vasanth117 <vasanthakumara117@gmail.com>
2021-10-06 19:45:35 +0530

    init

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ from web3 import Web3
 import json
 import web3
 
+
 ganache_url = "http://127.0.0.1:7545"
 web3 = Web3(Web3.HTTPProvider(ganache_url))
 web3.eth.default_account = web3.eth.accounts[0]
